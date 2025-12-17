79a38af151f14203716916e947464e2bebfd6742
Giovanne Morais <gmagenciamkte@gmail.com>
2025-12-17 16:43:34 -0300

    Create solutions.routes.tsx

---
+++ src/solutions.routes.tsx
@@ -0,0 +1,28 @@
+import { LeadGenerationPage } from "@/pages/solutions/LeadGenerationPage";
+import { EcommercePage } from "@/pages/solutions/EcommercePage";
+import { LandingPagesPage } from "@/pages/solutions/LandingPagesPage";
+import { GoogleBusinessPage } from "@/pages/solutions/GoogleBusinessPage";
+import { AIPage } from "@/pages/solutions/AIPage";
+
+export const solutionsRoutes = [
+  {
+    path: "/solucoes/geracao-de-leads-trafego-pago",
+    element: <LeadGenerationPage />,
+  },
+  {
+    path: "/solucoes/ecommerce-trafego-pago",
+    element: <EcommercePage />,
+  },
+  {
+    path: "/solucoes/landing-pages",
+    element: <LandingPagesPage />,
+  },
+  {
+    path: "/solucoes/google-meu-negocio",
+    element: <GoogleBusinessPage />,
+  },
+  {
+    path: "/solucoes/inteligencia-artificial",
+    element: <AIPage />,
+  },
+];
