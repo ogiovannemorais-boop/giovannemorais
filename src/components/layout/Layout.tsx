fb3fed4df53e5aa23a24848c78bccc3c884fb689
Giovanne Morais <gmagenciamkte@gmail.com>
2025-12-17 18:59:20 -0300

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { WhatsAppButton } from "./WhatsAppButton";
-import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
