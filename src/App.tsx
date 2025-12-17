3ded2b5d8d22314e3556b6519347aaea266300dc
Giovanne Morais <gmagenciamkte@gmail.com>
2025-12-17 18:11:28 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,25 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/layout/Layout";
+// Páginas principais
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import BlogPage from "./pages/BlogPage";
+import BlogPostPage from "./pages/BlogPostPage";
 import LinksPage from "./pages/LinksPage";
 import PrivacyPage from "./pages/PrivacyPage";
 import TermsPage from "./pages/TermsPage";
 import NotFound from "./pages/NotFound";
 
+// Soluções
+import LeadGenerationPage from "./pages/solutions/LeadGenerationPage";
+import EcommercePage from "./pages/solutions/EcommercePage";
+import LandingPagesPage from "./pages/solutions/LandingPagesPage";
+import GoogleBusinessPage from "./pages/solutions/GoogleBusinessPage";
+import AIPage from "./pages/solutions/AIPage";
+
+// Layout
+import Layout from "./components/layout/Layout";
 
 const queryClient = new QueryClient();
 
@@ -26,22 +37,53 @@ const App = () => (
           <Route path="*" element={
             <Layout>
               <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/sobre" element={<Sobre />} />
-                <Route path="/blog" element={<BlogPage />} />
-                <Route path="/blog/:slug" element={<BlogPostPage />} />
-                <Route path="/solucoes/geracao-de-leads-trafego-pago" element={<LeadGenerationPage />} />
-                <Route path="/solucoes/ecommerce-trafego-pago" element={<EcommercePage />} />
-                <Route path="/solucoes/landing-pages" element={<LandingPagesPage />} />
-                <Route path="/solucoes/google-meu-negocio" element={<GoogleBusinessPage />} />
-                <Route path="/solucoes/inteligencia-artificial" element={<AIPage />} />
-                <Route path="/politica-privacidade" element={<PrivacyPage />} />
-                <Route path="/termos" element={<TermsPage />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Layout>
-          } />
+  {/* Página isolada (fora do Layout) */}
+  <Route path="/links" element={<LinksPage />} />
+
+  {/* Páginas com Layout */}
+  <Route
+    path="*"
+    element={
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sobre" element={<AboutPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPostPage />} />
+
+          {/* Soluções */}
+          <Route
+            path="/solucoes/geracao-de-leads-trafego-pago"
+            element={<LeadGenerationPage />}
+          />
+          <Route
+            path="/solucoes/ecommerce-trafego-pago"
+            element={<EcommercePage />}
+          />
+          <Route
+            path="/solucoes/landing-pages"
+            element={<LandingPagesPage />}
+          />
+          <Route
+            path="/solucoes/google-meu-negocio"
+            element={<GoogleBusinessPage />}
+          />
+          <Route
+            path="/solucoes/inteligencia-artificial"
+            element={<AIPage />}
+          />
+
+          {/* Legais */}
+          <Route path="/politica-privacidade" element={<PrivacyPage />} />
+          <Route path="/termos" element={<TermsPage />} />
+
+          {/* 404 */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
+      </Layout>
+    }
+  />
+</Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
