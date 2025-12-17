5a085576a1056ada4a0b73d9fdb559ae7c882889
Giovanne Morais <gmagenciamkte@gmail.com>
2025-12-17 16:55:27 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/layout/Layout";
-import { HomePage } from "./pages/HomePage";
-import { AboutPage } from "./pages/AboutPage";
-import { BlogPage } from "./pages/BlogPage";
-import { BlogPostPage } from "./pages/BlogPostPage";
-import { LeadGenerationPage } from "./pages/solutions/LeadGenerationPage";
-import { EcommercePage } from "./pages/solutions/EcommercePage";
-import { LandingPagesPage } from "./pages/solutions/LandingPagesPage";
-import { GoogleBusinessPage } from "./pages/solutions/GoogleBusinessPage";
-import { AIPage } from "./pages/solutions/AIPage";
-import { PrivacyPage } from "./pages/PrivacyPage";
-import { TermsPage } from "./pages/TermsPage";
-import LinksPage from "./pages/LinksPage";
+import { solutionsRoutes } from "@/routes/solutions.routes";
 import NotFound from "./pages/NotFound";
+
 
 const queryClient = new QueryClient();
 
